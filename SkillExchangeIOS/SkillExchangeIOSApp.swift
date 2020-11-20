4139cbb17e538cb33646dab861ee810fa135a602
Muhammad AbdulSalam <abdulsalamch32@gmail.com>
2020-11-20 21:11:26 +0000

    Initial Commit

---
+++ SkillExchangeIOS/SkillExchangeIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillExchangeIOSApp.swift
+//  SkillExchangeIOS
+//
+//  Created by salam on 20/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SkillExchangeIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
