ace35df4166d634767829741db0133799207b17b
Muhammad AbdulSalam <abdulsalamch32@gmail.com>
2020-11-20 23:39:44 +0000

    UI changes

--- SkillExchangeIOS/ContentView.swift
+++ SkillExchangeIOS/ContentView.swift
@@ -8,10 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    // 1.
+       @State var isActive:Bool = false
+       
+       var body: some View {
+           VStack {
+               // 2.
+               if self.isActive {
+                   // 3.
+                    LoginView()
+               } else {
+                   // 4.
+                   Text("Awesome Splash Screen!")
+                       .font(Font.largeTitle)
+               }
+           }
+           // 5.
+           .onAppear {
+               // 6.
+               DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                   // 7.
+                   withAnimation {
+                       self.isActive = true
+                   }
+               }
+           }
+       }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- SkillExchangeIOS/LoginView.swift
+++ SkillExchangeIOS/LoginView.swift
@@ -7,10 +7,42 @@
 
 import SwiftUI
 
+
 struct LoginView: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        
+        Color.blue
+            .overlay(
+                VStack(alignment: .leading, spacing: 20) {
+                    
+                    HStack{
+                        Spacer()
+                        Image(systemName: "person.circle")
+                            .foregroundColor(Color.gray)
+                        Spacer()
+                        TextField("Enter username...", text: $username)
+                    }
+                    .padding(.vertical, 10)
+                    .background(RoundedCorners(color: .white, tl: 10, tr: 10, bl: 10, br: 10))
+
+                    
+                    HStack{
+                        Spacer()
+                        Image(systemName: "lock.shield")
+                            .foregroundColor(Color.gray)
+                        Spacer()
+                        TextField("Enter password...", text: $password)
+                    }
+                    .padding(.vertical, 10)
+                    .background(RoundedCorners(color: .white, tl: 10, tr: 10, bl: 10, br: 10))
+                    
+                        }.padding(.horizontal, 10)
+                    )
+            }
 }
 
 struct LoginView_Previews: PreviewProvider {
@@ -18,3 +50,6 @@ struct LoginView_Previews: PreviewProvider {
         LoginView()
     }
 }
+
+
+
