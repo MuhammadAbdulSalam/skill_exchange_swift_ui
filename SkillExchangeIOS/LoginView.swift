5be7c52a470acfe6cdedfaa425c48b92aa62293a
Muhammad AbdulSalam <abdulsalamch32@gmail.com>
2020-11-20 23:37:46 +0000

    initial commit

---
+++ SkillExchangeIOS/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  SkillExchangeIOS
+//
+//  Created by salam on 20/11/2020.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ SkillExchangeIOS/ViewUtility.swift
@@ -0,0 +1,20 @@
+//
+//  ViewUtility.swift
+//  SkillExchangeIOS
+//
+//  Created by salam on 20/11/2020.
+//
+
+import SwiftUI
+
+struct ViewUtility: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewUtility_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewUtility()
+    }
+}
